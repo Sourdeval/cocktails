d59150693fc2925b2b6f38389d7e5f2a5de09f6a
Sourdeval <sourdeval@laposte.net>
2023-08-12 22:47:58 +0200

    [Master] Delete party

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -4,7 +4,7 @@ import { environment } from 'src/environments/environment';
 
 import { FirebaseApp, initializeApp } from "firebase/app";
 import { browserSessionPersistence, getAuth, setPersistence, signInWithEmailAndPassword, signOut, UserCredential } from "firebase/auth";
-import { getFirestore, doc, getDoc, DocumentSnapshot, DocumentData, setDoc } from "firebase/firestore"; 
+import { getFirestore, doc, getDoc, DocumentSnapshot, DocumentData, setDoc, deleteDoc } from "firebase/firestore"; 
 import { Cocktail, Party, UserAccount } from './app.core';
 
 @Injectable({
@@ -17,6 +17,10 @@ export class BackendService {
     const auth = getAuth(this.firebaseApp)
     setPersistence(auth, browserSessionPersistence );
   }
+
+  private readonly COL_PARTY = 'parties';
+  private readonly COL_USER = 'users';
+  private readonly COL_COCKTAIL = 'cocktails';
 
   // Firebase Auth
   login(email: string, password: string) : Promise<UserCredential>{
@@ -36,7 +40,7 @@ export class BackendService {
   // Firestore
   getAccount(uid: string) : Promise<UserAccount>{
     return new Promise( (resolve, reject) => {
-      this.getData("users", uid).then(
+      this.getData(this.COL_USER, uid).then(
         data => {
           if (data.exists()){
             resolve(data.data() as UserAccount);
@@ -54,15 +58,27 @@ export class BackendService {
     return getDoc(docRef);
   }
 
+  private deleteData(collection: string, id: string) : Promise<void>{
+    const db = getFirestore(this.firebaseApp);
+    const docRef = doc(db, collection, id);
+    return deleteDoc(docRef);
+  }
+
+  private updateData(collection: string, id: string, data: any) : Promise<void>{
+    const db = getFirestore(this.firebaseApp);
+    const docRef = doc(db, collection, id);
+    return setDoc(docRef, data);
+  }
+
   createAccount(uid: string, name: string){
     const db = getFirestore(this.firebaseApp);
-    const accountRef = doc(db, "users", uid);
+    const accountRef = doc(db, this.COL_USER, uid);
     return setDoc(accountRef, { name: name }, { merge: true });
   }
 
   getParty(id: string) : Promise<Party>{
     return new Promise( (resolve, reject) => {
-      this.getData("parties", id).then(
+      this.getData(this.COL_PARTY, id).then(
         data => {
           if (data.exists()){
             resolve(data.data() as Party);
@@ -74,9 +90,23 @@ export class BackendService {
     });
   }
 
+  deleteParty(id: string, uid: string): Promise<void[]>{
+    return Promise.all([
+      this.deleteData(this.COL_PARTY, id),
+      new Promise<void>( (resolve, reject) => {
+        this.getAccount(uid).then(data => {
+          data.partiesId = data.partiesId.filter(partyId => {
+            return partyId !== id;
+          })
+          return this.updateData(this.COL_USER, uid, data);
+        });
+      })
+    ]);
+  }
+
   getCocktail(id: string) : Promise<Cocktail>{
     return new Promise( (resolve, reject) => {
-      this.getData("cocktails", id).then(
+      this.getData(this.COL_COCKTAIL, id).then(
         data => {
           if (data.exists()){
             resolve(data.data() as Cocktail);
